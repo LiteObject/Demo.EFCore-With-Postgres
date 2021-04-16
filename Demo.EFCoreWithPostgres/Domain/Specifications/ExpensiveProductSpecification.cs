2dcb9437c16de058cbe70a34758e32076352d055
Mohammed Hoque <mhoque@signifyhealth.com>
2021-04-16 10:45:30 -0500

    Added a new specification to show chaining of specs

---
+++ Demo.EFCoreWithPostgres/Domain/Specifications/ExpensiveProductSpecification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.EFCoreWithPostgres.Domain.Specifications
+{
+    public class ExpensiveProductSpecification: Specification<Product>
+    {
+        public override Expression<Func<Product, bool>> ToExpression() => product => product.UnitPrice > 100;
+    }
+}
